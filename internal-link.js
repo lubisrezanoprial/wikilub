46d70a50a6badde70d92f37512296433de45692e
lubisrezanoprial <145378518+lubisrezanoprial@users.noreply.github.com>
2023-09-24 22:19:46 +0700

    Update internal-link.js
    Tambah kata

--- internal-link.js
+++ internal-link.js
@@ -3,6 +3,10 @@
 var internalLinks = {
   "guru": "/2023/09/guru.html",
   "peserta didik": "/2023/09/peserta-didik.html",
+  "lembaga pendidikan": "/2023/09/lembaga-pendidikan.html",
+  "pendidikan formal": "/2023/09/pendidikan-formal.html",
+  "kepala sekolah": "/2023/09/kepala-sekolah.html",
+  "media pembelajaran": "/2023/09/media-pembelajaran.html",
   // tambahkan lebih banyak kata kunci dan URL sesuai kebutuhan
 };
 
